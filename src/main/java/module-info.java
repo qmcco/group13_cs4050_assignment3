542cd26d01b68ce8ef5e195e673f31b109df1d00
xmach <xq.machado04@gmail.com>
2025-03-19 14:26:54 -0500

    ReFactored The Package and Added MinHeap/MaxHeap

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module org.example.Trees {
+module group13_cs4050_assignment3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
 
 
-    opens org.example.Trees to javafx.fxml;
-    exports org.example.Trees;
+    opens group13_cs4050_assignment3 to javafx.fxml;
+    exports group13_cs4050_assignment3;
 }
