b1f39fa46a526b297254c869155ed4d98522b19d
xmach <xq.machado04@gmail.com>
2025-03-20 17:31:28 -0500

    Added Comments to the Max/Min Heap Classes

--- src/main/java/group13_cs4050_assignment3/MaxHeap.java
+++ src/main/java/group13_cs4050_assignment3/MaxHeap.java
@@ -3,38 +3,48 @@ package group13_cs4050_assignment3;
 import javafx.scene.paint.Color;
 
 public class MaxHeap<T extends Comparable<T>> extends Heap<T> {
+
+    // Moves Elements up the heap if necessary to maintain the max-heap
+    // This should be called after inserting a new element
     @Override
     protected void heapifyUp(int index) {
+        // Swaps the element with its parent if it is larger than the parent
         while (index > 0 && heap.get(index).compareTo(heap.get(getParentIndex(index))) > 0) {
             swap(index, getParentIndex(index));
             index = getParentIndex(index);
         }
     }
 
+    // Moves Elements down the heap if necessary to maintain the max-heap property
+    // This should be called after removing the root element
     @Override
     protected void heapifyDown(int index) {
         int largest = index;
         int left = getLeftChildIndex(index);
         int right = getRightChildIndex(index);
 
+        // Check left child for being larger
         if (left < heap.size() && heap.get(left).compareTo(heap.get(largest)) > 0) {
             largest = left;
         }
+        // Check right child for being larger
         if (right < heap.size() && heap.get(right).compareTo(heap.get(largest)) > 0) {
             largest = right;
         }
-
+        // If the largest needs to be changed, swap and recurse down the heap
         if (largest != index) {
             swap(index, largest);
             heapifyDown(largest);
         }
     }
 
+    // Returns the type as a string
     @Override
     public String type() {
         return "MaxHeap";
     }
 
+    // Overrides the color to make the nodes a different color than the others
     @Override
     public Color color() {
         return Color.BLUE;
--- src/main/java/group13_cs4050_assignment3/MinHeap.java
+++ src/main/java/group13_cs4050_assignment3/MinHeap.java
@@ -4,38 +4,45 @@ import javafx.scene.paint.Color;
 
 public class MinHeap<T extends Comparable<T>> extends Heap<T> {
 
+    // Moves Elements Up the heap if necessary to maintain the min-heap
+    // This should be called after inserting a new element
     @Override
     protected void heapifyUp(int index) {
+        // Swaps the element with its parent if it is smaller than the parent
         while (index > 0 && heap.get(index).compareTo(heap.get(getParentIndex(index))) < 0) {
             swap(index, getParentIndex(index));
             index = getParentIndex(index);
         }
     }
 
+    // Moves Elements Down the heap if necessary to maintain the min-heap
+    // This should be called after removing the root element
     @Override
     protected void heapifyDown(int index) {
         int smallest = index;
         int left = getLeftChildIndex(index);
         int right = getRightChildIndex(index);
 
+        // Check left child for being smaller
         if (left < heap.size() && heap.get(left).compareTo(heap.get(smallest)) < 0) {
             smallest = left;
         }
+        // Check right child for being smaller
         if (right < heap.size() && heap.get(right).compareTo(heap.get(smallest)) < 0) {
             smallest = right;
         }
-
+        // If the smallest needs to be changed then it swaps and recurses down the heap
         if (smallest != index) {
             swap(index, smallest);
             heapifyDown(smallest);
         }
     }
-
+    // Returns the type as a string
     @Override
     public String type() {
         return "MinHeap";
     }
-
+    // Overrides the color to make the nodes a different color than the others
     @Override
     public Color color() {
         return Color.GREEN;
